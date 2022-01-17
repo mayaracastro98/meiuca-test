f3fdff2c676a5327b996618ae3a67d6b4f24a5d2
Mayara <mayaraxs2@hotmail.com>
2022-01-17 01:20:16 -0300

    feat: adds tokens

---
+++ build/css/colors.ts
@@ -0,0 +1,19 @@
+import '../../build/css/variables.css'
+
+export enum Meiuca {
+    fontFamilyHighlighht = 'var(--font-family-highlight)',
+    fontWeightBold = 'var(--font-weight-bold)',
+    fontWeightMedium = 'var(--font-weight-medium)',
+    fontWeightRegular = 'var(--font-weight-regular)',
+    brandColorPrimary1 = 'var(--brand-color-primary-1)',
+    brandColorPrimary2 = 'var(--brand-color-primary-2)',
+    brandColorPrimary3 = 'var(--brand-color-primary-3)',
+    brandColorPrimary4 = 'var(--brand-color-primary-4)',
+    brandColorPrimary5 = 'var(--brand-color-primary-5)',
+    neutralColor1 = 'var(--neutral-color-1)',
+    neutralColor2 = 'var(--neutral-color-2)',
+    neutralColor3 = 'var(--neutral-color-3)',
+    neutralColor4 = 'var(--neutral-color-4)',
+    neutralColor5 = 'var(--neutral-color-5)',
+  }
+  
